f516f73edbd72f902d2a96c027dc6e3cfcdc1ef3
gopalkatariya <katariyagopal60@gmail.com>
2022-12-18 15:35:59 +0530

    ignore

--- main.py
+++ main.py
@@ -1,53 +1,13 @@
-from typing import Union
-
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 app = FastAPI()
 
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
+@app.get('/')
+def index():
+    return {'data': {'name': 'gopal'}}
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
-
-
-def get_full_name(first_name: str, last_name: str):
-    full_name = first_name.title() + " " + last_name.title()
-    return full_name
-
-
-print(get_full_name("john", "doe"))
-
-
-class Person:
-    def __init__(self, name: str):
-        self.name = name
-
-
-def get_person_name(one_person: Person):
-    return one_person.name
-
-
-p = Person("Gopal")
-print(get_person_name(one_person=p))
+@app.get('/data')
+def index():
+    return {'data': "hello..."}
